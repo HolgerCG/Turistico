1c0c95a673cb35cf704483db02a470ea38eb91c3
Holger Centeno <holgermcg@gmail.com>
2023-12-26 01:16:46 -0500

    Obtencion datos de la url con Retrofit Sacando los datos del JSON

--- app/src/main/java/com/example/turistico/MainActivity2.java
+++ app/src/main/java/com/example/turistico/MainActivity2.java
@@ -1,48 +1,118 @@
 package com.example.turistico;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+public class MainActivity2 extends AppCompatActivity {
 
-import java.util.HashMap;
-import java.util.Map;
+    public TextView txtListaBancos;
 
-import WebServices.Asynchtask;
-import WebServices.WebService;
-
-public class MainActivity2 extends AppCompatActivity implements Asynchtask {
+    public interface LugaresAPI {
+        @GET("lugar_turistico/json_getlistadoGrid")
+        Call<LugarResponse> getLugares();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Map<String, String> datos = new HashMap<String, String>();
 
-        WebService ws = new
-                WebService("https://uealecpeterson.net/turismo/lugar_turistico/json_getlistadoGrid",
-                datos,
-                MainActivity2.this,
-                MainActivity2.this);
+        txtListaBancos = findViewById(R.id.txtListaBancos);
 
-        ws.execute("GET");
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://uealecpeterson.net/turismo/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        LugaresAPI api = retrofit.create(LugaresAPI.class);
+
+        Call<LugarResponse> call = api.getLugares();
+
+        call.enqueue(new Callback<LugarResponse>() {
+            @Override
+            public void onResponse(Call<LugarResponse> call, Response<LugarResponse> response) {
+                if (response.isSuccessful()) {
+                    LugarResponse lugarResponse = response.body();
+                    if (lugarResponse != null) {
+                        mostrarNombresLugares(lugarResponse.getData());
+                    } else {
+                        Toast.makeText(MainActivity2.this,
+                                "Respuesta nula", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity2.this,
+                            "Respuesta no exitosa: " + response.code(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LugarResponse> call, Throwable t) {
+                Log.e("Error", t.getMessage());
+                Toast.makeText(MainActivity2.this,
+                        "Error al consultar API", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-
-    @Override
-    public void processFinish(String result) throws JSONException {
-        Log.d("API_RESPONSE", result);  // Imprime la respuesta en la consola
-
-        TextView txtBancos = findViewById(R.id.txtListaBancos);
-
-        // Muestra el resultado JSON completo en el TextView
-        txtBancos.setText("Respuesta WS: " + result);
+    private void mostrarNombresLugares(List<Lugar> lugares) {
+        StringBuilder nombres = new StringBuilder();
+        for (Lugar lugar : lugares) {
+            nombres.append("Categoria: "+lugar.getCategoria()+
+                    "//  Nombre: "+ lugar.getNombre_lugar()+
+            "//  Telefono :"+ lugar.getTelefono()).append("\n");
+        }
+        txtListaBancos.setText(nombres.toString());
     }
 
+    public class LugarResponse {
+        private List<Lugar> data;
+
+        public List<Lugar> getData() {
+            return data;
+        }
+
+        public void setData(List<Lugar> data) {
+            this.data = data;
+        }
+    }
+
+    public class Lugar {
+        private String categoria;
+        private String nombre_lugar;
+        private String telefono;
+
+        public String getCategoria() {
+            return categoria;
+        }
+
+        public void setCategoria(String categoria) {
+            this.categoria = categoria;
+        }
+
+        public String getNombre_lugar() {
+            return nombre_lugar;
+        }
+
+        public void setNombre_lugar(String nombre_lugar) {
+            this.nombre_lugar = nombre_lugar;
+        }
+
+        public String getTelefono() {
+            return telefono;
+        }
+
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+    }
 }
